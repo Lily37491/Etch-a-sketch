44775dfd667466389210c386d10438c5f972b9e7
Payal Kumavat <payalkumavat02@gmail.com>
2023-09-13 19:40:43 +0530

    Appen child to parent container & 1 div appears

---
+++ index.js
@@ -0,0 +1,10 @@
+const parentContainer = document.querySelector("#container");
+
+const childDiv = document.createElement('div');
+childDiv.classList.add('child-div');
+parentContainer.appendChild(childDiv);
+
+let rows = 16;
+let columns = 16;
+
+
