e04b881c9b6aab48462cf95b4e3e5df26950edcb
Payal Kumavat <payalkumavat02@gmail.com>
2023-09-14 16:49:12 +0530

    Set the limit of user input to maximum 100

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const parentContainer = document.querySelector("#container");
-let gridSize = 16;
+let userInput = prompt("Enter a Grid Size between 1 and 100", "67");
+let gridSize = parseInt(userInput);
+parentContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+parentContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+
+do {
+    userInput = prompt("Enter a Grid Size between 1 and 100", "67");
+    gridSize = parseInt(userInput);
+
+    if (gridSize > 100 || isNaN(gridSize) || gridSize < 1) {
+        alert("Please enter a valid grid size between 1 and 100.");
+    }
+} while (gridSize > 100 || isNaN(gridSize) || gridSize < 1);
+
 
 for (let i = 0; i < gridSize * gridSize; i++){
     const divs = document.createElement('div');
@@ -7,9 +20,7 @@ for (let i = 0; i < gridSize * gridSize; i++){
     parentContainer.appendChild(divs);
 
     divs.addEventListener('mouseover', () => {
-        divs.style.background = "blue";
+        divs.style.background = "black";
     })
  
 }
-
-
